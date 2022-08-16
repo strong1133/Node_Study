af78e9c9277473a086a6b9a2b0c9c6bfe5435d82
strong1133 <strong1133@naver.com>
2022-08-16 09:23:03 +0900

    post error handle

--- node_expert/memo/controllers/memo.js
+++ node_expert/memo/controllers/memo.js
@@ -11,6 +11,13 @@ const postTodo = async (req) => {
     console.log(req);
     try {
         const { value } = req;
+        
+        if(!value){
+            console.log("요청 값 부족");
+            let err = new Error('요청 값이 잘못되었습니다.')
+            err.code = 400;
+            return err;
+        }
 
         const maxOrderByUserId = await Todo.findOne().sort("-order").exec();
         const order = maxOrderByUserId ? maxOrderByUserId.order + 1 : 1;
--- node_expert/memo/routes/memo.js
+++ node_expert/memo/routes/memo.js
@@ -2,26 +2,29 @@ var express = require('express');
 var router = express.Router();
 const { responseDtoJson } = require("../utils/response_dto");
 
-const {testCtrl, postTodo} = require('../controllers/memo');
+const { testCtrl, postTodo } = require('../controllers/memo');
 
 
 
 router.get('/', function (req, res, next) {
-    let result =  testCtrl(req.query);
+    let result = testCtrl(req.query);
     console.log("heelo")
     responseDtoJson(null, null, result, res);
 });
 
 
-router.post('/', function (req, res, next) {
-    let result =  postTodo(req.body);
-    
-    if(result){
+router.post('/', async (req, res, next)  =>{
+    let result = await postTodo(req.body);
+
+    if (result == true) {
         responseDtoJson(null, null, 'Success', res);
-    }else{
-        responseDtoJson(null, 500, 'Fail', res);
+    }else {
+        responseDtoJson(result, result.code, null,res);
     }
-    
+
+
+
+
 });
 
 module.exports = router
