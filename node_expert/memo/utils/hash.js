b7c98d9ddecee73d83c83d4bf76a72b0ea23ca74
strong1133 <strong1133@naver.com>
2023-05-05 21:14:21 +0900

    .

--- node_expert/memo/utils/hash.js
+++ node_expert/memo/utils/hash.js
@@ -2,19 +2,11 @@ const SHA256 = require("crypto-js/sha256");
 const Secret = require("../config/secret.json");
 
 
-const hasher = (str)=>{
-    var hash =  SHA256(str+Secret['secretKey']);
+const hasher = (str) => {
+    var hash = SHA256(str + Secret['secretKey']);
     return hash.toString();
 }
-
-
 
 module.exports = {
     hasher
 }
-
-
-
-
-
-
